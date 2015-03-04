4659c97fa9dd432ad1bf040062a54fe80248da78
unknown <thwhite@697-334-Valmar.sf.k12.internal>
2015-03-04 09:57:08 -0800

    Basic player class def

--- build-tmp/source/Apotheosis.java
+++ build-tmp/source/Apotheosis.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Apotheosis extends PApplet {
+
 boolean mainMenu, gameState;
 Player eva;
 
@@ -189,3 +205,12 @@ AQUA: The Swarm.
 
 
 */
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Apotheosis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
